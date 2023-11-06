773f44d30e62c68258dbd43e78af083333a918be
IPECTER <ipectert@gmail.com>
2023-11-07 02:22:33 +0900

    [ UPDATE ] RTUBiomeLib 1.7.2 Change Version Detection

--- src/main/java/com/github/ipecter/rtu/biomelib/VersionUtil.java
+++ src/main/java/com/github/ipecter/rtu/biomelib/VersionUtil.java
@@ -10,14 +10,8 @@ public class VersionUtil {
 
     private static final Version VERSION = new Version(fromAPI(Bukkit.getVersion()));
 
-    private static String fromAPI(String version) {
+    public static String fromAPI(String version) {
         return version.split("-")[0];
-    }
-
-    public static boolean isSupportVersion(String maxVersion, String minVersion) {
-        Version max = new Version(fromAPI(maxVersion));
-        Version min = new Version(fromAPI(minVersion));
-        return max.getVersion() >= VERSION.getVersion() && min.getVersion() <= VERSION.getVersion();
     }
 
     public static Version getVersion() {
@@ -33,13 +27,6 @@ public class VersionUtil {
             String majorStr = numbers.length == 0 ? "0" : numbers[0];
             String minorStr = numbers.length <= 1 ? "0" : numbers[1];
             String patchStr = numbers.length <= 2 ? "0" : numbers[2];
-            this.version = Integer.parseInt(majorStr + minorStr + patchStr);
-        }
-
-        public Version(String major, String minor, String patch) {
-            String majorStr = (String)Objects.requireNonNullElse(major, "0");
-            String minorStr = (String)Objects.requireNonNullElse(minor, "0");
-            String patchStr = (String)Objects.requireNonNullElse(patch, "0");
             this.version = Integer.parseInt(majorStr + minorStr + patchStr);
         }
 
